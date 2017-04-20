10cb6529023714659eafbd1cac530ad8397b280e
don <donfreiday@email.com>
2017-04-20 12:59:03 -0400

    Separated addition algorithm design code into its own function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 
+//void testASCIIAdditon();
+
 int main() {
 	/*int4k i1;
 	int4k i2(12345);
@@ -16,6 +18,16 @@ int main() {
 	i1 += i2;
 	cout << i1 << endl;
 
+
+
+
+	system("pause");
+	return 0;
+}
+
+
+/*
+void testASCIIAdditon() {
 	// Test ASCII addition algorithm
 	char lhs[4];
 	char rhs[4];
@@ -26,15 +38,15 @@ int main() {
 		sum[i] = 0;
 	}
 	sum[4] = 0;
-	
+
 	// OK, let's add 2 and 3.
 	lhs[0] = 2;
 	rhs[0] = 3;
 
 	__asm {
-		mov esi, 3 ;// esi is index;
-		mov edi, 4 ;//
-		mov ecx, 4 ;//
+		mov esi, 3;// esi is index;
+		mov edi, 4;//
+		mov ecx, 4;//
 		mov bh, 0;// set carry value to zero;
 	L1:;
 		mov ah, 0;// clear ah before addition;
@@ -50,21 +62,18 @@ int main() {
 		; or bh, 30h;// convert carry to ascii;
 		; or al, 30h;// convert carry1 back to ascii;
 		mov sum[edi], al;// save it in sum;
-		
+
 		or bh, 30h;// convert carry to ascii;
 		or al, 30h;// convert carry1 back to ascii;
 
 		dec esi;// back up one digit;
 		dec edi;
 		loop L1;
-		
+
 		sub bh, 30h;// convert back to decimal;
 		mov sum[edi], bh;//save last carry digit
 
 
 	}
-
-
-	system("pause");
-	return 0;
 }
+*/
