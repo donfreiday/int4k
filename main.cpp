6105bc8f7e8fb330e36c1ccf596525ef4240eb9a
donfreiday <donfreiday@gmail.com>
2017-05-02 20:14:01 -0400

    Removed 'using namespace std' from main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,11 @@
 #include "int4k.h"
 #include <iostream>
-using namespace std;
 
 int main() {
-	//int4k i1(1000);
-	//for (int i = 999; i > 0; i--)
-	//	i1 *= i;
-	//cout << i1 << endl;
-
-	int4k i1;
-	int4k i2;
-	cin >> i1 >> i2;
-	//i1 /= i2;
-	//cout << i1 << endl;
-	//cout << i1 << endl;
-	if (i1 != i2) 
-		cout << i1 << " != " << i2<<endl;
-
-
-	/*int4k i1;
-	int4k i2;
-	cin >> i1 >> i2;
-	cout << i1 * i2 << endl;*/
-
+	int4k i1(1000);
+	for (int i = 999; i > 0; i--)
+		i1 *= i;
+	std::cout << i1 << std::endl;
 	system("pause");
 	return 0;
 }
